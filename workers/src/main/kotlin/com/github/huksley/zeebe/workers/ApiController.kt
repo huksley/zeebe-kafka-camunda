dd07a6b48dc775ed89dfe3c64cdc7a05ba533657
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2018-11-26 20:14:20 +0200

    Added workflow deployment and instance creation to workers project

---
+++ workers/src/main/kotlin/com/github/huksley/zeebe/workers/ApiController.kt
@@ -0,0 +1,13 @@
+package com.github.huksley.zeebe.workers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ApiController {
+
+    @GetMapping("/status")
+    fun status(): String {
+        return "OK"
+    }
+}
